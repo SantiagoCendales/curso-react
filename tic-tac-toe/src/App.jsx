46ed577c096dca75caf5e87262578f7e3805186c
Santiago Cendales <srcendales94@gmail.com>
2023-04-26 12:36:14 -0500

    Add change turn and winner check

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -6,10 +6,25 @@ const TURNS = {
   o: 'o'
 }
 
-const Square = ({ children, updateBoard, index, isSelected, handleTurn }) => {
+// eslint-disable-next-line no-unused-vars
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [3, 4, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const Square = ({ children, updateBoard, index, isSelected }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
   return (
-    <div className={className} onClick={() => handleTurn()}>
+    <div className={className} onClick={handleClick}>
       {children}
     </div>
   )
@@ -19,10 +34,39 @@ function App() {
   const initialBoardState = Array(9).fill(null)
   const [board, setBoard] = useState(initialBoardState);
   const [turn, setTurn] = useState(TURNS.x)
+  // null no hay ganador, false empate y true ganador
+  const [winner, setWinner] = useState(null);
 
-  const handleTurn = () => {
+  const updateBoard = (index) => {
+    // Se verifica que la casilla ya este llena
+    // o si ya hay un ganador
+    if(board[index] || winner) return
+    // Se actualiza el estado del board
+    const newBoard = [...board];
+    newBoard[index] = turn
+    setBoard(newBoard);
+    // Se cambia el turno
     const nextTurn = turn === TURNS.o ? TURNS.x : TURNS.o
     setTurn(nextTurn)
+    // Revisión de si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
+  }
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
   }
 
   return (
@@ -32,7 +76,11 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <Square key={index} index={index} handleTurn={handleTurn}>
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
                 {board[index]}
               </Square>
             )
